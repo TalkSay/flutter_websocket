b7b057685fac99cde32208482ed9eab0b07c0f02
Iampato <858pwaweru@gmail.com>
2022-12-14 13:11:15 +0300

    wired in an example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_websocket/flutter_websocket.dart';
+import 'package:flutter_websocket/models/connect_close_model.dart';
+import 'package:flutter_websocket/utils/flutter_socket_util.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +20,9 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  String _url = "";
   final _flutterWebsocketPlugin = FlutterWebsocket();
+  final _socketClient = FlutterWebSocketUtil();
 
   @override
   void initState() {
@@ -31,8 +36,8 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _flutterWebsocketPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      platformVersion = await _flutterWebsocketPlugin.getPlatformVersion() ??
+          'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -54,9 +59,78 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text('Running on: $_platformVersion\n'),
+            ),
+            customButton(
+              "Connect",
+              () {
+                _socketClient.connect(
+                  _url,
+                  onClose: (SocketConnectCloseModel detail) {
+                    if (kDebugMode) {
+                      print('ONCLOSE');
+                      print(detail.toString());
+                      print('ONCLOSE');
+                    }
+                  },
+                  onMessage: (String? message) {
+                    if (kDebugMode) {
+                      print('ON MESSAGE');
+                      print(message);
+                      print('ON MESSAGE');
+                    }
+                  },
+                  onOpen: (String? url) {
+                    if (kDebugMode) {
+                      print('ON OPEN');
+                      print(url);
+                      print('ON OPEN');
+                    }
+                  },
+                  onError: (String? message) {
+                    if (kDebugMode) {
+                      print('ON ERROR');
+                      print(message);
+                      print('ON ERROR');
+                    }
+                  },
+                );
+              },
+            ),
+            customButton(
+              "Disconnect",
+              () {},
+            ),
+            customButton(
+              "Open heartbeat",
+              () {},
+            ),
+            customButton(
+              "Is connected?",
+              () {},
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget customButton(String button, VoidCallback onTap) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 5,
+      ),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(double.infinity, 45),
+        ),
+        child: Text(button),
       ),
     );
   }
